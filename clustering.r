0776f4eb7ab524171adaa273842870406e39978b
aigerimmmm <46799842+aigerimmmm@users.noreply.github.com>
2020-12-28 22:39:14 -0800

    Create clustering.r

---
+++ clustering.r
@@ -0,0 +1,52 @@
+# developer activities csv file
+activity <- read.csv(file = "/data/activities.csv", header = TRUE)
+#column names of activities of developers csv file
+colnames(activity)
+head(activity)
+
+library(plyr)
+
+# duration column convert to date and time with an associated time zone
+activity$duration = as.POSIXct(activity$duration, format="%H:%M:%S")
+# function that calculates activity hour and activity duration in each session
+
+ActivityData <- ddply(activity,
+                      .(idesessionuuid, activewindow), function(x){
+                        ActDur = sum(as.numeric(as.POSIXct(x[, "duration"])), na.rm = T)
+                        ActHour = mean(as.numeric(format(as.POSIXct(x[, "triggeredat"], format="%H:%M:%S"),"%H")))
+                        return(c(ActHour=ActHour, ActDur=ActDur))
+                      })
+# column names of activitie's data with activity hour and activity duration
+colnames(ActivityData)
+head(ActivityData)
+
+
+# developer edits data
+edits <- read.csv(file = "/data/edits.csv", header = TRUE)
+colnames(edits)
+edits$duration = as.POSIXct(edits$duration, format="%H:%M:%S")
+EditDat <- ddply(edits,
+                 .(idesessionuuid, activewindow), function(x){
+                   EdtDur = sum(as.numeric(as.POSIXct(x[, "duration"])), na.rm = T)
+                   EdtNoChang = sum(x[, "numberchanges"])
+                   return(c(EdtDur=EdtDur, EdtNoChang=EdtNoChang))
+                 })
+colnames(EditDat)
+
+# Merge
+head(ActDat)
+head(EditDat)
+
+FnlDat <- merge(ActivityData, EditDat, by = c("idesessionuuid", "activewindow"))
+dim(FnlDat)
+head(FnlDat)
+colnames(FnlDat)
+
+
+# Clustering
+Clust2 <- kmeans(FnlDat[, -c(1 : 2)], centers = 2)
+plot(FnlDat[, -c(1 : 2)], col = Clust2$cluster)
+points(Clust2$centers, col = 1:5, pch = 8)
+
+Clust21 <- kmeans(FnlDat[, c(3 : 4)], centers = 2)
+plot(FnlDat[, c(3 : 4)], col = Clust21$cluster)
